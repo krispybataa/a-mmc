92d39e34536e6d9f369956d5b791e80eeab8c7a0
andyrews <john.andrew.gacho@gmail.com>
2026-04-03 12:40:29 +0800

    Reformated engine URI

--- a-mmc_backend/config/DevelopmentConfig.py
+++ a-mmc_backend/config/DevelopmentConfig.py
@@ -9,4 +9,15 @@ class DevelopmentConfig(BaseConfig):
     DEBUG: bool = True
     # Cookies do not require HTTPS over localhost
     JWT_COOKIE_SECURE: bool = False
-    SQLALCHEMY_DATABASE_URI: str = os.environ.get("SQLALCHEMY_DATABASE_URI") or os.environ.get("DATABASE_URL")
+    database_url = os.environ.get("DATABASE_URL")
+
+    if database_url:
+        SQLALCHEMY_DATABASE_URI = database_url
+    else:
+        SQLALCHEMY_DATABASE_URI = (
+            f"postgresql://{os.getenv('POSTGRES_USER')}:"
+            f"{os.getenv('POSTGRES_PASSWORD')}@"
+            f"{os.getenv('POSTGRES_HOST')}:"
+            f"{os.getenv('POSTGRES_PORT')}/"
+            f"{os.getenv('POSTGRES_DB')}"
+        )
