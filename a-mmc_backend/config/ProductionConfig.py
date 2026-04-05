17c1c1b973c895bf4109780fcd8ea08616698a46
andyrews <john.andrew.gacho@gmail.com>
2026-04-05 17:40:33 +0800

    Modified for actions and railway

--- a-mmc_backend/config/ProductionConfig.py
+++ a-mmc_backend/config/ProductionConfig.py
@@ -7,15 +7,15 @@ load_dotenv()
 
 class ProductionConfig(BaseConfig):
     DEBUG: bool = False
-    database_url = os.environ.get("DATABASE_URL")
+    database_url = os.environ.get("ACTIONS_TEST_DATABASE_URL")
 
     if database_url:
         SQLALCHEMY_DATABASE_URI = database_url
     else:
         SQLALCHEMY_DATABASE_URI = (
-            f"postgresql://{os.getenv('POSTGRES_USER')}:"
-            f"{os.getenv('POSTGRES_PASSWORD')}@"
-            f"{os.getenv('POSTGRES_HOST')}:"
-            f"{os.getenv('POSTGRES_PORT')}/"
-            f"{os.getenv('POSTGRES_DB')}"
-        )
+    f"postgresql://{os.getenv('PGUSER')}:"
+    f"{os.getenv('PGPASSWORD')}@"
+    f"{os.getenv('PGHOST')}:"
+    f"{os.getenv('PGPORT', '5432')}/"
+    f"{os.getenv('PGDATABASE')}"
+)
